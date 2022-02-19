25de12f1c561e62447454c978aa9ebf4a203e636
Akshad tiwari <akshad113@gmail.com>
2022-02-19 11:34:51 +0530

    create the schema

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+mongoose.connect( 'mongodb://localhost/codeial_development');
+
+const db= mongoose.connection;
+
+
+db.on('err',console.error.bind(console,'Error while connecting to MongoDB'));
+
+
+
+db.once('open',function(){
+    console.log('Connected to Database :: MongoDB');
+})
+
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose')
 
-//use express router
-app.use('/',require("./routes"));
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 
+//using express router
+app.use('/',require("./routes")); 
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+  
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
