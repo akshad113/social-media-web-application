3ceb8f9c5a5b7acae81b3efdd77e0886c53bd908
Akshad tiwari <akshad113@gmail.com>
2022-02-20 19:55:29 +0530

    postsSchema has been cerated working on show post on website

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
  
-const postSchema = new.mongoose.Schema({
+const postSchema = new mongoose.Schema({
     content:{
         type:String,
         required: true
