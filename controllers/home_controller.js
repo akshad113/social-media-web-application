1c8163aa293eb24c4d8a57449f0d83490e82ae69
Akshad tiwari <akshad113@gmail.com>
2022-02-26 08:48:24 +0530

    convert the code in ajax

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,19 +3,10 @@ const User = require('../models/user');
 
 
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
-
-    // populate the user of each post
-    Post.find({})
+module.exports.home = async function(req, res){
+    try{
+         // populate the user of each post
+    let posts = await Post.find({})
     .populate('user')
     .populate({
         path: 'comments',
@@ -23,18 +14,25 @@ module.exports.home = function(req, res){
             path: 'user'
         }
     })
-    .exec(function(err, posts){
+    
 
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users: users
-            });
-        });
+    let users = await User.find({})
 
-       
-    })
+ 
+    return res.render('home', {
+        title: "Codeial | Home",
+        posts:  posts,
+        all_users: users
+    });
+
+    }catch(err){
+       console.log("Error",err);
+       return; 
+    }   
+    
+    
+    
+   
 
 }
 
